c0257939101a8f44beac17cf7b2e9295676bf3b1
Vistonho <86853661+Vistonho@users.noreply.github.com>
2021-09-09 22:03:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const image =  document.querySelector('https://www.belanta.vet/vet-blog/wp-content/uploads/2019/09/1-6.jpg');
+    image.classList.add('animated', 'pulse');
+});
