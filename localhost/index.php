402a865bedd2a51801ae3a004f0590986fb5c0d0
Vistonho <86853661+Vistonho@users.noreply.github.com>
2021-09-21 09:51:26 +0300

    Add files via upload

---
+++ localhost/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE>
+<head>
+	<link rel="stylesheet" href="master.css">
+	<link rel="stylesheet" href="slider.css">
+</head>
+<body>
+<header>
+	<title>My-Giro</title>
+	<div class="logo">
+		<img src="img/Logo.png" class="logo-img">
+	</div>
+	<div class="telephone">
+		<p class="phone">+7 351 111 22 33</p>
+		<p><a href="tel:+73511112233"><img class="pbutton" src="img/трубка.png" alt="Телефон"></a></p>
+	</div>
+</header>
+
+<!-- блок слайдера -->
+<div class="carousel-wrapper">
+	<span id="item-1"></span>
+	<span id="item-2"></span>
+	<span id="item-3"></span>
+	<span id="item-4"></span>
+	<!-- 1й элемент слайдера -->
+	<div class="carousel-item item-1">
+		<p>Мы обслуживаем одинаково качественно как своих гарантийных
+клиентов, так и сторонние заказы.</p>
+		<a class="arrow arrow-prev" href="#item-4"></a>
+		<a class="arrow arrow-next" href="#item-2"></a>
+	</div>
+	<!-- 2й элемент слайдера -->
+	<div class="carousel-item item-2">
+		<p>Если деталь сломалась — в нашем центре ее не будут ремонтировать,
+а сразу заменят на новую.</p>
+		<a class="arrow arrow-prev" href="#item-1"></a>
+		<a class="arrow arrow-next" href="#item-3"></a>
+	</div>
+	<!-- 3й элемент слайдера -->
+	<div class="carousel-item item-3">
+		<p>Мы работаем с крупной сетью сервисных центров, ее филиалы
+находятся в 70 городах России.</p>
+		<a class="arrow arrow-prev" href="#item-2"></a>
+		<a class="arrow arrow-next" href="#item-4"></a>
+	</div>
+	<!-- 4й элемент слайдера -->
+	<div class="carousel-item item-4">
+		<p>Гарантия действует в течении 3 лет!</p>
+		<a class="arrow arrow-prev" href="#item-3"></a>
+		<a class="arrow arrow-next" href="#item-1"></a>
+	</div>
+</div>
+
+
+<!--Блок, содержащий особенности-->
+<div class="wrapper"> <!-- Блоки с картинками и текстом -->
+	<div class="part"> <!-- Электросамокат -->
+		<img class="tech-img" src="img/elsam.png" alt="Самокат">
+		<div class="text">
+			<p> 	Электросамокат - современный вариант простого самоката.
+				Для того чтобы на нем кататься, нужно всего один раз оттолкнуться ногой от земли, обеспечив стартовое ускорение.
+				Дальше устройство будет везти хозяина самостоятельно за счет встроенного электромотора.
+				Если выбирать между моноколесом или электросамокатом, последний будет гораздо безопаснее в эксплуатации за счет наличия 2 колес и ручкек для опоры руками.</p>
+		</div>
+	</div>
+	<div class="part"> <!-- Гироскутер -->
+		<img class="tech-img" src="img/giro.png" alt="Самокат">
+		<div class="text">
+			<p> 	Гироскутер представляет собой конструкцию из 2 колес,
+				расположенных по бокам от наездника и соединенных поперечной
+				планкой-корпусом. Внутри корпуса располагаются
+				электродвигатели, аккумуляторные батареи и гироскопы,
+				поддерживающие в горизонтальном положении площадку для ног.
+				Гироскутеры никогда не оснащаются рукоятками для рук.
+				Устройства из 2 колес, поперечной площадки и рулевого столба
+				называются сигвеями и управляются манипуляциями руками.
+				Особенность управления гироскутером состоит в том, что для
+				движения вперед наезднику необходимо наклониться вперед. Для
+				торможения или движения назад - наклониться назад. Для поворота
+				нужно наклонять корпус в правую или левую сторону.</p>
+		</div>
+	</div>
+	<div class="part"> <!-- Сигвей -->
+		<img class="tech-img" src="img/sig.png" alt="Самокат">
+		<div class="text">
+			<p> 	Сигвей - средство передвижения для одного человека, которое
+				держит равновесие с помощью специальной автобалансирующей
+				системы. Это своеобразный электрический скутер, самостоятельно
+				удерживающий равновесие. Сигвеи бесшумны в работе, не
+				загрязняют окружающую среду, работают на электрической энергии,
+				накопленной батареей аккумуляторов.</p>
+		</div>
+	</div>
+</div>
+
+<div class="h1">
+	<h1>Продаваемые модели</h1>
+</div>
+
+<div class="tovary">
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i1" src="img/tov1.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Гироскутер</p>
+			<p class="model">Smart Balance 6.5" Ver. 1</p>
+			<p class="ru">8990 рублей</p>	
+		</div>
+	</div>
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i2" src="img/tov2.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Гироскутер</p>
+			<p class="model">KIWANO KO-X ES03</p>
+			<p class="ru">18900 рублей</p>
+		</div>
+	</div>
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i3" src="img/tov4.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Cигвей</p>
+			<p class="model">Minirobot Mini PRO 36V Белый</p>
+			<p class="ru">15990 рублей</p>
+		</div>
+	</div>
+</div>
+
+
+
+<div class="tovary">
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i4" src="img/tov4.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Cигвей</p>
+			<p class="model">Xiaomi Ninebot Mini</p>
+			<p class="ru">22900 рублей</p>
+		</div>
+	</div>
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i5" src="img/tov4.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Сигвей</p>
+			<p class="model">Hili E+</p>
+			<p class="ru">39900 рублей</p>
+		</div>
+	</div>
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i6" src="img/tov7.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Электросамокат</p>
+			<p class="model">Yamato PES MINI</p>
+			<p class="ru">15900 рублей</p>
+		</div>
+	</div>
+</div>
+
+
+<div class="tovary">
+	<div class="tov-number">
+		<div class="tov-img">
+			<img id="i7" src="img/tov7.jpg" alt="Smart Balance 6.5" Ver. 1">
+		</div>
+		<div class="tov-text">
+			<p class="name">Электросамокат</p>
+			<p class="model">KUGOO S2 (Jilong)</p>
+			<p class="ru">14900 рублей</p>
+		</div>
+	</div>
+</div>
+
+<div class="h1">
+	<h1>Наши cотрудники - наша гордость!</h1>
+</div>
+
+<div class="sotrud" >
+	<div class="tabl">
+		<p>Иванов Иван Иванович – квалифицированный специалист сервисного
+центра My Gyro.</p>
+	</div>
+	<div class="rab">
+		<img src="img/Гоблин3.jpg" alt="Гоблин">
+	</div>
+</div>
+
+<div class="sotrud" >
+	<div class="rab">
+		<img src="img/Гоблин.png" alt="Гоблин">
+	</div>
+	<div class="tabl">
+		<p>Пупкин Василий Васильевич – старший консультант My Gyro.</p>
+	</div>
+</div>
+
+<div class="sotrud" >
+<div class="tabl">
+		<p>Захаров Андрей Борисович – менеджер-консультант My Gyro.</p>
+	</div>
+	<div class="rab">
+		<img src="img/Гоблин2.png" alt="Гоблин">
+	</div>
+</div>
+
+</body>
